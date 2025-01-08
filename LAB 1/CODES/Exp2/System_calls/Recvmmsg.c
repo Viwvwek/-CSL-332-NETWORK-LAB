cc7d2ec131fc89990f07ffdf010fd5d57a139492
Vivek Vinod <116374299+Viwvwek@users.noreply.github.com>
2025-01-08 11:34:07 +0530

    Update Recvmmsg.c

--- LAB 1/CODES/Exp2/System_calls/Recvmmsg.c
+++ LAB 1/CODES/Exp2/System_calls/Recvmmsg.c
@@ -1,4 +1,4 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-ssize_t recv(int sockfd, void *buf, size_t len, int flags);
+int recvmmsg(int sockfd, struct mmsghdr *msgvec, unsigned int vlen, int flags, struct timespec *timeout);
