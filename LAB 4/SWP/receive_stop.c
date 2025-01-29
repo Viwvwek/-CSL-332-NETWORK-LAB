4c404c7b93c1585a7b393d2030b70d39ff071b31
Vivek Vinod <116374299+Viwvwek@users.noreply.github.com>
2025-01-29 10:21:17 +0530

    Update receive_stop.c

--- LAB 4/SWP/receive_stop.c
+++ LAB 4/SWP/receive_stop.c
@@ -5,6 +5,9 @@
 #include <unistd.h>     // UNIX standard functions (for close)
 
 int main(void) {
+
+    int k=5,m=1,p;
+    char buffer[1024];
     // Declaration of variables
     int socket_desc; // Socket descriptor for the client
     struct sockaddr_in server_addr; // Structure for server address
@@ -33,3 +36,49 @@ int main(void) {
         return -1; // Exit if connection fails
     }
     printf("Connected to server successfully!\n");
+
+    while(k!=0){
+        if(m<=5){
+            printf("Sending frame!\n");
+        }
+        if(m%2==0)
+        {
+            printf("sending is successfull!\n");
+        }
+        else{
+            strcpy(buffer,"error");
+            printf("packet loss!\n");
+            for(p=1;p<=3;p++)
+            {
+                printf("Waiting for %d seconds\n",p);
+            }
+            printf("Retransmitting.....\n");
+            strcpy(buffer,"frame");
+            sleep(3);
+        }
+
+        int y = send(socket_desc,buffer,19,0);
+        if(y==-1)
+        {
+            printf("Error in sending");
+            exit(1);
+        }
+        else
+        {
+            printf("Sent frame %d\n",m);
+
+        }
+        int z = recv(socket_desc,buffer,1024,0);
+        if(z==-1)
+        {
+            printf("Error in Receiving data: ");
+        }
+        k--;
+        m++;
+
+    }
+    close(socket_desc);
+    return 0;
+
+
+}
