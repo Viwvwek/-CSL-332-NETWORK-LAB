d33d0080218f1fbb65c65b4ff27c57dbbab5dfc6
Neil <neilseneasow@gmail.com>
2025-04-06 12:05:58 +0530

    modify dvr

--- DVR/dvr.c
+++ DVR/dvr.c
@@ -1,56 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct node {
-    unsigned dist[20];
-    unsigned from[20];
-} rt[10];
+int d[10][10], via[10][10];
 
 int main() {
-    int costmat[20][20];
-    int nodes, i, j, k, count = 0;
+    int i, j, k, n, g[10][10];
 
-    printf("Enter the number of nodes: ");
-    scanf("%d", &nodes);
+    printf("\nEnter the number of nodes: ");
+    scanf("%d", &n);
 
-    printf("Enter the cost matrix: \n");
-    for (i = 0; i < nodes; i++) {
-        for (j = 0; j < nodes; j++) {
-            printf("costmat[%d][%d]: ", i, j);
-            scanf("%d", &costmat[i][j]);
-        }
-        costmat[i][i] = 0;  // Ensure diagonal elements are 0
-    }
-
-    // Initialize distance table
-    for (i = 0; i < nodes; i++) {
-        for (j = 0; j < nodes; j++) {
-            rt[i].dist[j] = costmat[i][j];
-            rt[i].from[j] = j;
+    printf("\n--- Enter the cost matrix (Use 999 for no direct path) ---\n");
+    for (i = 0; i < n; i++) {
+        printf("\nFor node %c:\n", i + 97);
+        for (j = 0; j < n; j++) {
+            printf("Cost from %c to %c: ", i + 97, j + 97);
+            scanf("%d", &g[i][j]);
+            if (g[i][j] != 999)
+                d[i][j] = 1;
         }
     }
 
-    // Distance Vector Algorithm
-    do {
-        count = 0;
-        for (i = 0; i < nodes; i++) {
-            for (j = 0; j < nodes; j++) {
-                for (k = 0; k < nodes; k++) {
-                    if (rt[i].dist[j] > costmat[i][k] + rt[k].dist[j]) {
-                        rt[i].dist[j] = costmat[i][k] + rt[k].dist[j];
-                        rt[i].from[j] = k;
-                        count++;
+    // Display initial cost matrix
+    printf("\n--- Initial Cost Matrix ---\n    ");
+    for (i = 0; i < n; i++) {
+        printf("%4c", i + 97);
+    }
+    printf("\n");
+    for (i = 0; i < n; i++) {
+        printf("%c |", i + 97);
+        for (j = 0; j < n; j++) {
+            if (g[i][j] == 999)
+                printf(" INF");
+            else
+                printf("%4d", g[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Initialize via matrix
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            via[i][j] = i;
+
+    // Distance vector routing calculation
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (d[i][j] == 1) {
+                for (k = 0; k < n; k++) {
+                    if (g[i][j] + g[j][k] < g[i][k]) {
+                        g[i][k] = g[i][j] + g[j][k];
+                        via[i][k] = j;
                     }
                 }
             }
         }
-    } while (count != 0);
+    }
 
-    // Print Routing Tables
-    for (i = 0; i < nodes; i++) {
-        printf("\nRouting Table for Router %d\n", i + 1);
-        printf("Destination\tNext Hop\tDistance\n");
-        for (j = 0; j < nodes; j++) {
-            printf("%d\t\t%d\t\t%d\n", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
+    // Final output
+    printf("\n--- Final Routing Table ---\n");
+    for (i = 0; i < n; i++) {
+        printf("\nRouting table for node %c:\n", i + 97);
+        printf("Destination\tCost\tVia\n");
+        for (j = 0; j < n; j++) {
+            printf("    %c\t\t", j + 97);
+            if (g[i][j] == 999)
+                printf("INF\t-");
+            else
+                printf("%d\t%c", g[i][j], via[i][j] + 97);
+            printf("\n");
         }
     }
 
